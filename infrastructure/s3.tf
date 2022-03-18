fda60ed0b63952f6010ca8119ec3a8c6e08597aa
Júlia Morais <julia.morais@a3data.com.br>
2022-03-17 21:01:44 -0300

    change datalake name on PR

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "datalake" {
   # Parâmetros de configuração do recurso escolhido
-  bucket = "${var.base-bucket-name}-${var.account-number}"
+  bucket = "${var.base-bucket-name}-${var.account-number}-prod"
   # acl    = "private"
 
 
