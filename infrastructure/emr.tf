f5ce8b48f0bea0ed2896e88cf6bccd3d83a673c7
Júlia Morais <julia.morais@a3data.com.br>
2022-03-29 17:45:06 -0300

    fix emr module

--- infrastructure/emr.tf
+++ infrastructure/emr.tf
@@ -15,12 +15,12 @@ EOF
   termination_protection            = false
   keep_job_flow_alive_when_no_steps = true
 
-  ec2_attributes {
-    subnet_id                         = aws_subnet.main.id
-    emr_managed_master_security_group = aws_security_group.sg.id
-    emr_managed_slave_security_group  = aws_security_group.sg.id
-    instance_profile                  = aws_iam_instance_profile.emr_profile.arn
-  }
+  # ec2_attributes {
+  #   subnet_id                         = aws_subnet.main.id
+  #   emr_managed_master_security_group = aws_security_group.sg.id
+  #   emr_managed_slave_security_group  = aws_security_group.sg.id
+  #   instance_profile                  = aws_iam_instance_profile.emr_profile.arn
+  # }
 
   master_instance_group {
     instance_type = "m5.xlarge"
